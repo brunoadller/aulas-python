8f409ee7d2656345dbe1eea0c8d1ac3dafa3c004
brunoadller <adller676@gmail.com>
2025-09-23 16:44:09 -0300

    send pasta condicao if, pacote random e pacote statistics

---
+++ pacote_random/main.py
@@ -0,0 +1,15 @@
+import random
+import math
+
+Lista = [1,2,3,4,5,6,7,8]
+print(random.choice(Lista))
+
+
+palavra = "bruno"
+
+print(random.choice(palavra))
+
+print(random.random())
+print(math.floor(random.random()))
+
+print(random.randint(0, 50))
---
+++ pacote_statistics/main.py
@@ -0,0 +1,9 @@
+import statistics
+
+
+lista = [12, 15,28,56,78,80, 28]
+print(sum(lista)/len(lista))
+
+print(statistics.mean(lista))
+print(statistics.median(lista))
+print(statistics.mode(lista))
