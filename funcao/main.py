bdcbcef715bc92e880706db14206a75ce368e880
brunoadller <adller676@gmail.com>
2025-09-25 16:37:41 -0300

    send funcao

--- funcao/main.py
+++ funcao/main.py
@@ -1,5 +1,14 @@
 
-def boas_vindas():
-    print('Python.....................')
+def boas_vindas(mensagem):
+    print('Python.....................', mensagem)
 
 
+
+boas_vindas("Olá mundo")
+
+
+def somar(valor_1, valor_2):
+    soma = valor_1 + valor_2
+    print(soma)
+
+somar(2,3)
